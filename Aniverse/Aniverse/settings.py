eb5173ed026cae25cdbd7f10b0e2fa97f6f0c8cc
Vishal Singh <133119527+Vishal18cse@users.noreply.github.com>
2024-03-11 21:25:02 +0530

    deployment to railway

--- Aniverse/Aniverse/settings.py
+++ Aniverse/Aniverse/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ez^cq)rcl!6xz$cg)cou=fs&@+h%3i6fa3$z5c=0e1$%_g__-=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
